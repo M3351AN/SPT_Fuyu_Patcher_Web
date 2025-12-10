d4afa213c97af7a60e6ddd4b76cb9d1e84d28a55
渟雲 <QuQ@outlook.it>
2025-12-10 17:34:40 +0800

    opti: optimiz page loading performance

--- Program.cs
+++ Program.cs
@@ -16,13 +16,92 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SPT.Fuyu.Patcher.Blazor;
 using SPT.Fuyu.Patcher.Blazor.Services;
+using System.Buffers;
+using System.Diagnostics;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+{
+    var httpClient = new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    };
+
+    return httpClient;
+});
+
 builder.Services.AddScoped<ExeAnalyzerService>();
 builder.Services.AddScoped<JsInteropService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+_ = Task.Run(async () =>
+{
+    await Task.Delay(500);
+    await LightweightWarmupAsync(host);
+});
+
+await host.RunAsync();
+
+static async Task LightweightWarmupAsync(WebAssemblyHost host)
+{
+    try
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        var warmupTasks = new List<Task>
+        {
+            Task.Run(() => PreloadService<ExeAnalyzerService>(host)),
+            Task.Run(() => PreloadService<JsInteropService>(host))
+        };
+
+        var dotnetWarmup = Task.Run(() =>
+        {
+            try
+            {
+                _ = new MemoryStream(64);
+                _ = typeof(List<byte>);
+                _ = typeof(Dictionary<string, object>);
+                _ = ArrayPool<byte>.Shared.Rent(1);
+                _ = BitConverter.ToString(new byte[] { 0x01 });
+            }
+            catch {
+            }
+        });
+
+        warmupTasks.Add(dotnetWarmup);
+
+        var combinedTask = Task.WhenAll(warmupTasks);
+        var timeoutTask = Task.Delay(TimeSpan.FromMilliseconds(150));
+
+        await Task.WhenAny(combinedTask, timeoutTask);
+
+        stopwatch.Stop();
+        Console.WriteLine($"Warmuped: {stopwatch.ElapsedMilliseconds}ms");
+    }
+    catch
+    {
+    }
+}
+
+static void PreloadService<T>(WebAssemblyHost host) where T : class
+{
+    try
+    {
+        var service = host.Services.GetService<T>();
+        if (service != null)
+        {
+            if (service is ExeAnalyzerService analyzer)
+            {
+                _ = analyzer.GetType().GetProperty("ByteArrayPool")?.GetValue(null);
+            }
+        }
+    }
+    catch
+    {
+    }
+}
